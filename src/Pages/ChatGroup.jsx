5f35587466cf0f3fd1e80ff0b1e6555e89e02da8
neerajnightcoder <kumar.neeraj.developer@gmail.com>
2024-03-07 18:52:38 +0530

    group chat

--- src/Pages/ChatGroup.jsx
+++ src/Pages/ChatGroup.jsx
@@ -27,15 +27,18 @@ function ChatGroup() {
       <h1 className="text-xl font-bold mb-4">Available Chat Rooms</h1>
       <ul>
         {rooms.map((room) => (
-          <li key={room.id} className="mb-2">
+          <li key={room.id} className="mb-2 bg-slate-900 p-3 rounded-lg">
             <Link
-              to={`/chat-room/${room.id}`}
-              className="text-blue-600 hover:underline"
+              to={`/chat-room/${room.id}?roomName=${room.name}`}
+              className="text-blue-600 hover:underline "
             >
-              {room.name}
+              {room.name}{" "}
+              <span className=" text-cyan-300">({room.members.length})</span>
             </Link>
-            {room.isLocked && (
-              <span className="ml-2 text-red-500"> (Password Required)</span>
+            {room.isLocked ? (
+              <div className=" text-red-500">"Password Required"</div>
+            ) : (
+              <div className=" text-cyan-500">"Open Group"</div>
             )}
           </li>
         ))}
--- src/Pages/GroupChatPage.jsx
+++ src/Pages/GroupChatPage.jsx
@@ -1,41 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useSocket } from "../context/SocketContext";
 
 const ChatRoomPage = () => {
   const { roomId } = useParams();
+  const [insideRoom, setInsideRoom] = useState(false);
   const [message, setMessage] = useState("");
   const [chatMessages, setChatMessages] = useState([]);
+  const [password, setPassword] = useState("");
+  const [isPasswordRequired, setIsPasswordRequired] = useState(false);
   const { socket } = useSocket();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const roomName = new URLSearchParams(location.search).get("roomName");
 
   useEffect(() => {
-    // Function to join the room
     const joinRoom = () => {
-      // Send a request to the server to join the room
       socket.emit("joinRoom", { roomId });
-
-      // Listen for success response from the server
       socket.on("joinRoomSuccess", ({ room }) => {
         console.log("Joined room:", room);
+        setInsideRoom(true);
+        socket.on("message", (msg) => {
+          console.log("message", msg, insideRoom);
+          console.log("inside room");
+          setChatMessages((prevMessages) => [...prevMessages, msg]);
+        });
       });
 
-      // Listen for error response from the server
+      socket.on("joinRoomError", ({ message }) => {
+        if (message === "Incorrect password") setIsPasswordRequired(true); // Set flag if the room is password-protected
+      });
       socket.on("joinRoomError", ({ message }) => {
         console.error("Error joining room:", message);
       });
     };
 
     if (socket) {
-      // Join the room when the component mounts
       joinRoom();
-
-      // Listen for incoming messages
-      socket.on("message", (msg) => {
-        setChatMessages((prevMessages) => [...prevMessages, msg]);
-      });
     }
 
-    // Cleanup function
     return () => {
       if (socket) {
         socket.off("message");
@@ -44,41 +47,92 @@ const ChatRoomPage = () => {
   }, [roomId, socket]);
 
   const handleSendMessage = () => {
-    if (socket && message) {
+    if (socket && message && insideRoom) {
       socket.emit("sendGroupMessage", { message, room: roomId });
       setMessage("");
     }
   };
+
+  const handleJoinRoom = () => {
+    socket.emit("joinRoom", { roomId, password });
+    socket.on("joinRoomSuccess", ({ room }) => {
+      console.log("Joined room:", room);
+      setInsideRoom(true);
+      socket.on("message", (msg) => {
+        console.log("message", msg, insideRoom);
+        console.log("inside room");
+        setChatMessages((prevMessages) => [...prevMessages, msg]);
+      });
+    });
+
+    socket.on("joinRoomError", ({ message }) => {
+      if (message === "Incorrect password") setIsPasswordRequired(true); // Set flag if the room is password-protected
+    });
+  };
+
+  const handleCancelJoin = () => {
+    navigate("/"); // Go back to the previous page if the user cancels joining the room
+  };
+
   return (
     <div className="max-w-md mx-auto mt-8 p-4 bg-white rounded-lg shadow-lg">
-      <h1 className="text-xl font-bold mb-4">Chat Room {roomId}</h1>
-      <div>
-        {chatMessages.map((msg, index) => (
-          <div
-            key={index}
-            className={`${
-              msg.sender === socket.id ? "bg-blue-100" : "bg-gray-100"
-            } px-3 py-2 mb-2 rounded-lg`}
-          >
-            {msg.message}
+      <h1 className="text-xl font-bold mb-4">
+        {insideRoom ? roomName : "Enter Password for " + roomName}
+      </h1>
+      {insideRoom && (
+        <>
+          <div>
+            {chatMessages.map((msg, index) => (
+              <div
+                key={index}
+                className={`${
+                  msg.sender === socket.id ? "bg-blue-100" : "bg-gray-100"
+                } px-3 py-2 mb-2 rounded-lg`}
+              >
+                {msg.message}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex items-center mt-4">
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message..."
-          className="flex-1 mr-2 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-        />
-        <button
-          onClick={handleSendMessage}
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none"
-        >
-          Send
-        </button>
-      </div>
+          <div className="flex items-center mt-4">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type your message..."
+              className="flex-1 mr-2 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            />
+            <button
+              onClick={handleSendMessage}
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none"
+            >
+              Send
+            </button>
+          </div>
+        </>
+      )}
+      {isPasswordRequired && !insideRoom && (
+        <div className="mt-4 flex">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter room password..."
+            className="mr-2 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+          />
+          <button
+            onClick={handleJoinRoom}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg focus:outline-none"
+          >
+            Join Room
+          </button>
+          <button
+            onClick={handleCancelJoin}
+            className="px-4 py-2 bg-gray-500 text-white rounded-lg focus:outline-none"
+          >
+            Cancel
+          </button>
+        </div>
+      )}
     </div>
   );
 };
